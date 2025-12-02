5b0646e662516f69537a7167a9ca3e0b56a38c0e
Elektron1kk <228fedyapupkin228@gmail.com>
2025-12-02 22:11:56 +0900

    first practies about peremennye

---
+++ cousre-1/practies/c1-pt-1/02.12_lvl1/02.12_lvl1/Program.cs
@@ -0,0 +1,53 @@
+﻿//using System.ComponentModel.Design;
+
+//int num = 10;
+//if (num > 0)
+//{
+//    Console.WriteLine("это положительное число");
+//}
+//else if (num < 0)
+//{
+//    Console.WriteLine("это отрицательное число");
+//}
+//else
+//    Console.WriteLine("это число 0");
+
+//using System.ComponentModel.Design;
+
+//string a = Console.ReadLine();
+//int age = Convert.ToInt32(a);
+//if (age >= 18)
+//{
+//    Console.WriteLine("ты большой");
+//}
+//else
+//
+//Console.WriteLine("ты звиздюк");
+
+
+//int num = 7;
+//int a = (7 % 2);
+//if (a == 1)
+//{
+//    Console.WriteLine("Число нечетное");
+//}
+//else
+//    Console.WriteLine("Число четное");
+
+int a = 5;
+int b = -2;
+double first = Convert.ToDouble(a);
+double second = Convert.ToDouble(b);
+if (first && second > 0)
+{
+    Console.WriteLine("Числа положительные");
+}
+else if (first || second > 0)
+{
+    Console.WriteLine("тут хотя бы одно положительное");
+}
+else (first < 0);
+{
+    Console.WriteLine("а не положительное");
+}
+
